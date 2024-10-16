245a58c0a21f78bb9b82837f1cf08b2e1ed5a480
glaceon033 <glaceon033@gmail.com>
2024-10-16 09:31:55 +0700

    day 16: coffee machine oop

---
+++ day16-oop-coffeemachine/day16-oop.py
@@ -0,0 +1,31 @@
+# from turtle import *
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("DarkSeaGreen4")
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.field_names = ["Pokedex No.", "Pokemon", "Type"]
+table.add_rows(
+    [
+        ["#001", "Chespin", "Grass"],
+        ["#002", "Quilladin", "Grass"],
+        ["#003", "Chesnaught", "Grass - Fighting"],
+        ["#004", "Fennekin", "Fire"],
+        ["#005", "Braixen", "Fire"],
+        ["#006", "Delphox", "Fire - Psychic"],
+        ["#007", "Froakie", "Water"],
+        ["#008", "Frogadier", "Water"],
+        ["#009", "Greninja", "Water - Dark"],
+    ]
+)
+
+table.align["Pokemon"] = "l"
+table.align["Type"] = "l"
+
+print(table)
